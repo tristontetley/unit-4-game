811f4d5095b7c7ea770702f40dca86a22260527e
Triston Tetley <tetleylax@aol.com>
2019-10-22 18:41:20 -0400

    I add id's to many elements in html to be able to call them when needed in my html

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+let blue = document.getElementById("blue");
+let seeThru = document.getElementById("seeThru");
+let darkPurple = document.getElementById("darkPurple");
+let yellow = document.getElementById("yellow");
