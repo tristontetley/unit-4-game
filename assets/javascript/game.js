727641a94919c3fd9486cb7e649945045aa46db0
Triston Tetley <tetleylax@aol.com>
2019-10-23 16:56:30 -0400

    got images linked to .html and fixed jquery link issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,41 @@
-let cpuTotal = Math.floor(Math.random() * 30);
-let wins = 0;
-let losses = 0;
-let userTotal = 0;
+$(document).ready(function () {
+    let cpuTotal = Math.floor(Math.random() * 40) + 30;
+    let wins = 0;
+    let losses = 0;
+    let userTotal = 0;
+
+    let blueCrystal = Math.floor(Math.random() * 12) + 1;
+    let clearCrystal = Math.floor(Math.random() * 12) + 1;
+    let purpleCrystal = Math.floor(Math.random() * 12) + 1;
+    let yellowCrystal = Math.floor(Math.random() * 12) + 1;
+
+    $(".btn1").on("click", function () {
+        userTotal = userTotal + blueCrystal;
+        $("#user-total").text(userTotal);
+    });
 
 
-blueCrystal = Math.floor(Math.random()*12)+1;
-clearCrystal= Math.floor(Math.random()*12)+1;
-purpleCrystal= Math.floor(Math.random()*12)+1;
-yellowCrystal= Math.floor(Math.random()*12)+1;
+    $(".btn2").on("click", function () {
+        userTotal += "clearCrystal";
+    })
+    $(".btn3").on("click", function () {
+        userTotal += "purpleCrystal";
+    })
+    $(".btn4").on("click", function () {
+        userTotal += "yellowCrystal";
+    });
 
-$(".btn1").click(function () {
-    userTotal += "blueCrystal";
+    if (userTotal === cpuTotal) {
+        wins++;
+        $("#wins").text(wins);
+    }
+    else if (userTotal > cpuTotal) {
+        losses++;
+        $("#losses").text(losses);
+    };
+
+
+
+
+
 });
-$(".btn2").click(function () {
-    userTotal += "clearCrystal";
-});
-$(".btn3").click(function () {
-    userTotal += "purpleCrystal";
-});
-$(".btn4").click(function () {
-    userTotal += "yellowCrystal";
-});
-
-if (userTotal === cpuTotal){
-    wins ++;
-    $("#wins").text(wins);
-};
-else if (userTotal > cpuTotal){
-    losses ++;
-    $("#losses").text(losses);
-};
-
-
-
-
