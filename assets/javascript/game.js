3c6780938bdd0d814bf1d2d1a9efd25954bbfff3
Triston Tetley <tetleylax@aol.com>
2019-10-23 02:08:40 -0400

    still need to add img tags too .html and get cpuTotal to display using jquery/js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,36 @@
-let blue = document.getElementById("blue");
-let seeThru = document.getElementById("seeThru");
-let darkPurple = document.getElementById("darkPurple");
-let yellow = document.getElementById("yellow");
+let cpuTotal = Math.floor(Math.random() * 30);
+let wins = 0;
+let losses = 0;
+let userTotal = 0;
+
+
+blueCrystal = Math.floor(Math.random()*12)+1;
+clearCrystal= Math.floor(Math.random()*12)+1;
+purpleCrystal= Math.floor(Math.random()*12)+1;
+yellowCrystal= Math.floor(Math.random()*12)+1;
+
+$(".btn1").click(function () {
+    userTotal += "blueCrystal";
+});
+$(".btn2").click(function () {
+    userTotal += "clearCrystal";
+});
+$(".btn3").click(function () {
+    userTotal += "purpleCrystal";
+});
+$(".btn4").click(function () {
+    userTotal += "yellowCrystal";
+});
+
+if (userTotal === cpuTotal){
+    wins ++;
+    $("#wins").text(wins);
+};
+else if (userTotal > cpuTotal){
+    losses ++;
+    $("#losses").text(losses);
+};
+
+
+
+
