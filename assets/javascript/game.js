11259e713c48ce82583010dda4ffe3dcfe84d634
Triston Tetley <tetleylax@aol.com>
2019-10-23 21:15:55 -0400

    finished crystal collector game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     let cpuTotal = Math.floor(Math.random() * 40) + 30;
+    $("#cpu-total").text(cpuTotal);
+
     let wins = 0;
     let losses = 0;
     let userTotal = 0;
@@ -9,33 +11,44 @@ $(document).ready(function () {
     let purpleCrystal = Math.floor(Math.random() * 12) + 1;
     let yellowCrystal = Math.floor(Math.random() * 12) + 1;
 
-    $(".btn1").on("click", function () {
-        userTotal = userTotal + blueCrystal;
+
+    $(".crystal").on("click", function () {
+        if ($(this).hasClass("btn1")) {
+            userTotal += blueCrystal;
+        }
+        if ($(this).hasClass("btn2")) {
+            userTotal += clearCrystal;
+        }
+        if ($(this).hasClass("btn3")) {
+            userTotal += purpleCrystal;
+        }
+        if ($(this).hasClass("btn4")) {
+            userTotal += yellowCrystal;
+        }
+        if (userTotal >= cpuTotal) {
+            if (userTotal == cpuTotal) {
+                wins++;
+                $("#wins").text(wins);
+            } else {
+                losses++;
+                $("#losses").text(losses);
+            }
+            userTotal = 0;
+            cpuTotal = Math.floor(Math.random() * 40) + 30;
+            blueCrystal = Math.floor(Math.random() * 12) + 1;
+            clearCrystal = Math.floor(Math.random() * 12) + 1;
+            purpleCrystal = Math.floor(Math.random() * 12) + 1;
+            yellowCrystal = Math.floor(Math.random() * 12) + 1;
+            
+        }
+        $("#cpu-total").text(cpuTotal);
         $("#user-total").text(userTotal);
+
     });
 
 
-    $(".btn2").on("click", function () {
-        userTotal += "clearCrystal";
-    })
-    $(".btn3").on("click", function () {
-        userTotal += "purpleCrystal";
-    })
-    $(".btn4").on("click", function () {
-        userTotal += "yellowCrystal";
-    });
-
-    if (userTotal === cpuTotal) {
-        wins++;
-        $("#wins").text(wins);
-    }
-    else if (userTotal > cpuTotal) {
-        losses++;
-        $("#losses").text(losses);
-    };
 
 
 
 
-
-});
+})
